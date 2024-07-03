38440e8080c2eed838305a3d5caa6eb7ce7c3166
thekey213 <azdilinga@gmail.com>
2024-07-03 21:09:22 +0200

    Did a little fixing

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import './App.css'; // Assuming you have a corresponding App.css for styling
 import Homepage from './pages/home/Homepage';
 import Gallery from './pages/gallery/Gallery';
 import Upload from './pages/upload/Upload';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router >
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/upload" element={<Upload />} />
-    
-   </Routes>
-  </Router>
+    <Router basename="/Milani">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/upload" element={<Upload />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-
