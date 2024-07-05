9b085c9628091d9a5b225af040af76939d0f05b2
thekey213 <azdilinga@gmail.com>
2024-07-05 19:58:22 +0200

    Testing action

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,18 +1,17 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyAzqOMYd3QXsX26iyojXfICz_U4e2UKRSM",
+    authDomain: "birdappproject.firebaseapp.com",
+    databaseURL: "https://birdappproject-default-rtdb.firebaseio.com",
+    projectId: "birdappproject",
+    storageBucket: "birdappproject.appspot.com",
+    messagingSenderId: "764585228279",
+    appId: "1:764585228279:web:e44dfa5e2451c45f58e0f2",
+    measurementId: "G-P986W0JEN9"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
